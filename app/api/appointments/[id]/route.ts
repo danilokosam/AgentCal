7f5f350415279a5c2a14aab5429afe1c74aad909
Daniel Duarte <danielduartemora27@gmail.com>
2026-03-26 19:29:54 +0100

    fix: force dynamic api route

--- app/api/appointments/[id]/route.ts
+++ app/api/appointments/[id]/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 import { getAppointment, cancelAppointment } from "@/services/appointmentService";
 
